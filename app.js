8801727b12dcca6a9c26389abe7fb58e2324f861
liaoshuang <345324985@qq.com>
2020-12-18 16:26:13 +0800

    list

--- app.js
+++ app.js
@@ -128,40 +128,58 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);   
   } else if(received_message.text === 'list'){
     response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "list",
-          "top_element_style": "compact",
-          "elements": [
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"receipt",
+          "recipient_name":"Stephane Crozatier",
+          "order_number":"12345678902",
+          "currency":"USD",
+          "payment_method":"Visa 2345",        
+          "order_url":"https://facete.herokuapp.com/index.html",
+          "timestamp":"1428444852",         
+          "address":{
+            "street_1":"1 Hacker Way",
+            "street_2":"",
+            "city":"Menlo Park",
+            "postal_code":"94025",
+            "state":"CA",
+            "country":"US"
+          },
+          "summary":{
+            "subtotal":75.00,
+            "shipping_cost":4.95,
+            "total_tax":6.19,
+            "total_cost":56.14
+          },
+          "adjustments":[
             {
-              "title": "Classic White T-Shirt",
-              "subtitle": "See all our colors",
-              "image_url": "https://facete.herokuapp.com/logo.png",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://facete.herokuapp.com",
-                "messenger_extensions": false,
-                "webview_height_ratio": "tall"
-              }
-            },{
-              "title": "Classic White T-Shirt",
-              "subtitle": "See all our colors",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://facete.herokuapp.com",
-                "messenger_extensions": false,
-                "webview_height_ratio": "tall"
-              }
+              "name":"New Customer Discount",
+              "amount":20
             },
-          ],
-           "buttons": [
             {
-              "title": "View More",
-              "type": "postback",
-              "payload": "payload"            
+              "name":"$10 Off Coupon",
+              "amount":10
             }
-          ]  
+          ],
+          "elements":[
+            {
+              "title":"Classic White T-Shirt",
+              "subtitle":"100% Soft and Luxurious Cotton",
+              "quantity":2,
+              "price":50,
+              "currency":"USD",
+              "image_url":"https://facete.herokuapp.com/styles/female-work.jpg"
+            },
+            {
+              "title":"Classic Gray T-Shirt",
+              "subtitle":"100% Soft and Luxurious Cotton",
+              "quantity":1,
+              "price":25,
+              "currency":"USD",
+              "image_url":"https://facete.herokuapp.com/styles/female-work.jpg"
+            }
+          ]
         }
       }
     }
