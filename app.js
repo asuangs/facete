2bf1c7d13ab4356e7063e23e8789234b12c7c445
liaoshuang <345324985@qq.com>
2020-12-18 14:36:04 +0800

    test

--- app.js
+++ app.js
@@ -122,24 +122,69 @@ function handleMessage(sender_psid, received_message) {
         }
       }
     }
-  } else if(received_message.text === 'vedio'){
+  } else if(received_message.text === 'list'){
     response = {
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type":"open_graph",
-          "elements":[
-             {
-              "url":"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
-              "buttons":[
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "list",
+          "top_element_style": "compact",
+          "elements": [
+            {
+              "title": "Classic T-Shirt Collection",
+              "subtitle": "See all our colors",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+              "buttons": [
                 {
-                  "type":"web_url",
-                  "url":"https://en.wikipedia.org/wiki/Rickrolling",
-                  "title":"View More"
-                }              
-              ]      
+                  "title": "View",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]
+            },
+            {
+              "title": "Classic White T-Shirt",
+              "subtitle": "See all our colors",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall"
+              }
+            },
+            {
+              "title": "Classic Blue T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              },
+              "buttons": [
+                {
+                  "title": "Shop Now",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]        
             }
-          ]
+          ],
+           "buttons": [
+            {
+              "title": "View More",
+              "type": "postback",
+              "payload": "payload"            
+            }
+          ]  
         }
       }
     }
